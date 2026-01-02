768e20f494474e7ff1bf7d5a88d9f5c590378ade
Creapson <creapson.creapson@gmail.com>
2026-01-02 14:41:09 +0100

    fixed error with shared param lists between elements

--- Element.py
+++ Element.py
@@ -2,11 +2,11 @@ from typing import List
 
 
 class Element:
-    def __init__(self, name=None, connections=[], type=None, param_dict={}):
+    def __init__(self, name=None, connections=[], type=None):
         self.name: str = name
         self.connections: List[str] = connections
         self.type: str = type
-        self.params: dict[str, str] = param_dict
+        self.params: dict[str, str] = {}
 
     def set_type(self, newType: str):
         self.type = newType
