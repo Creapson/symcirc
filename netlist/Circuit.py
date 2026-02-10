f1ea5f381bd2fd737e93d2a322670b432606a590
KMatipa <kevin.matipa@outlook.de>
2026-02-10 21:35:55 +0100

    fixed sherman morrison

--- netlist/Circuit.py
+++ netlist/Circuit.py
@@ -11,7 +11,7 @@ class Circuit:
 
         self.inner_connecting_nodes: List[str] = []
 
-        self.bipolar_model = "beta_with_r_be"
+        self.bipolar_model = "beta_with_r_be_G"
         self.mosfet_model = "BSIM"
 
         self.nodes: List[str] = []
