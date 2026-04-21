added "get" for sorting and elimintion methods, fixed calculation of transfer function in numerical solver

Modified_Node_Analysis.py
@@ -392,7 +392,7 @@ class ModifiedNodalAnalysis(EquationFormulator):
     
 
 
-    def solveNumerical(self, value_dict, frequencies, idx_out): 
+    def solveNumerical(self, value_dict, frequencies, idx_out, idx_in): 
 
         """Solve the equation system numerically based on the value dictionary.
 
@@ -422,8 +422,8 @@ class ModifiedNodalAnalysis(EquationFormulator):
             z_num_eval = z_num_func(s_val)
             x = scipy.solve(A_num_eval, z_num_eval)
 
-            return x[idx_out]
-            #H[k] = x[idx_out] #/ x[idx_in]
+            return x[idx_out] / x[idx_in]
+            
 
         H = np.array([solve_freq(freq) for freq in frequencies])
 

calc_tests.py
@@ -37,7 +37,7 @@ idx_in = x_syms.index(sp.symbols("V_1"))
 idx_out = x_syms.index(sp.symbols("V_10"))
 f = np.logspace(0, 7, 800)
 
-H_lambdified = mna.solveNumerical(mna.value_dict, f, idx_out)
+H_lambdified = mna.solveNumerical(mna.value_dict, f, idx_out, idx_in)
 
 
 print("\n\n\n\n\n\n")
@@ -56,13 +56,15 @@ print("Original transfer function:")
 print("\n\n\n\n\n\n")
 print("Approximation results:")
 ap = Approximation(mna)
+print("Available elimination methods:", ap.get_Elimination_Methods())
+print("Available sorting methods:", ap.get_Sorting_Methods())
 t0 = t.perf_counter_ns()
 #approximate(self, in_var, out_var, points/errors, term_removal_method, tolerance (tbt- rel_error; block - jmp_threshold), sorting_criterion, sorting_extra_var(column - col_num))
-approx = ap.approximate(sp.symbols('V_1'), sp.symbols('V_10'), ((1e5,0.05),), "tbt",0.6, "max", 1)
+approx = ap.approximate(sp.symbols('V_1'), sp.symbols('V_10'), ((1e5,0.05),), "term-by-term",0.6, "max", 1)
 t1 = t.perf_counter_ns()
 print(f"Time for approximation: {(t1 - t0) / 1e6} ms")
 
-approx_H_lambdified = approx.solveNumerical(mna.value_dict, f, idx_out)
+approx_H_lambdified = approx.solveNumerical(mna.value_dict, f, idx_out, idx_in)
 
 
 