e2255ca41d15aa82a2cf250e3b00ccbf5bbd2d88
Creapson <creapson.creapson@gmail.com>
2026-07-14 23:40:40 +0200

    user_manuel innitial commit

---
+++ docs/user_manual/ger/code/compiling.py
@@ -0,0 +1,7 @@
+# Installation des Enviroments
+python -m pip install poetry
+python -m poetry install
+
+# Installation von  pyinstaller und Compilation des Projekts
+python -m pip install pyinstaller
+python -m pip pyinstaller -F main.py
